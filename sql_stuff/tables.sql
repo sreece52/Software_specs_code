6ecb3aeb119a2b2cd9b889073e6c9ec22b87e4c0
sreece52 <shawnjreece21@hotmail.com>
2016-03-29 00:57:53 -0400

    Updated SQL
    The correct SQL to rebuild the data base.

--- sql_stuff/tables.sql
+++ sql_stuff/tables.sql
@@ -1,21 +1,22 @@
-
-create table Jobs(
-work_id int not null AUTO_INCREMENT,
-job_name varchar(50),
-fname varchar(20),
-lname varchar(20),
-street varchar(50),
-city varchar(25),
-state varchar(20),
-zip_code varchar(5),
-phone_num varchar(12),
-materials varchar(120),
-date varchar(10),
-hours int,
-startTime varchar(6),
-endTime varChar(6),
-notes varChar(120),
-pdfs varchar(20),
-images varchar(20),
-primary key(work_id)
-) 
+CREATE TABLE JOBS(
+    WORK_ID INT NOT NULL AUTO_INCREMENT,
+    JOB_NAME VARCHAR(50),
+    FNAME VARCHAR(20),
+    LNAME VARCHAR(20),
+    STREET VARCHAR(50),
+    CITY VARCHAR(25),
+    STATE VARCHAR(20),
+    ZIP_CODE VARCHAR(5),
+    PHONE_NUM VARCHAR(12),
+    MATERIALS VARCHAR(120),
+    DATE VARCHAR(10),
+    HOURS REAL,
+    STARTTIME VARCHAR(20),
+    ENDTIME VARCHAR(20),
+    NOTES VARCHAR(120),
+    PDFS VARCHAR(100),
+    IMAGES VARCHAR(100),
+    STARTAM BOOLEAN,
+    ENDAM BOOLEAN,
+    PRIMARY KEY(WORK_ID)
+)		
